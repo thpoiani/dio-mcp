6330e8c07923b2da6324227e12a6d04ae430e7cf
Thiago Poiani <thpoiani@gmail.com>
2025-08-17 16:54:40 -0600

    fix: remove código não usado

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,12 @@
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
-// Cria o MCP server
 const server = new McpServer({
   name: "dio-mcp-server",
   version: "1.0.0"
 });
 
-// Adiciona uma ferramenta de soma
-server.registerTool(
-  "add",
-  {
-    title: "Addition Tool",
-    description: "Add two numbers",
-    inputSchema: { a: z.number(), b: z.number() }
-  },
-  async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }]
-  })
-);
-
-// Adiciona um recurso dinâmico de saudação
-server.registerResource(
-  "greeting",
-  new ResourceTemplate("greeting://{name}", { list: undefined }),
-  {
-    title: "Greeting Resource",
-    description: "Dynamic greeting generator"
-  },
-  async (uri, { name }) => ({
-    contents: [{
-      uri: uri.href,
-      text: `Olá, ${name}!`
-    }]
-  })
-);
-
-// Adiciona uma ferramenta para buscar drinks
 server.registerTool(
   "fetch-drink",
   {
@@ -54,6 +23,5 @@ server.registerTool(
   }
 );
 
-// Inicia recebimento de mensagens via stdio
 const transport = new StdioServerTransport();
 await server.connect(transport);
